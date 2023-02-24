c1d7506a3718d88bc785f558596da2aecc0cc107
Harsh <harshvj03@gmail.com>
2023-02-25 00:29:07 +0530

    added code for portfolio

---
+++ src/Components/Card.js
@@ -0,0 +1,170 @@
+import React from 'react'
+import './Card.css';
+
+function Card() {
+    return (
+        <div className="card-list">
+            <article className="card">
+                <div className="card-header">
+                    <p>Sep 11th 2020</p>
+                    <h2>Never forget</h2>
+                </div>
+
+                <div className="card-author">
+                    <a className="author-avatar" href="#">
+                        <img src="https://scontent.fbho5-1.fna.fbcdn.net/v/t1.6435-9/168066427_1895907330549568_6597227817058193863_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=PjuyJsAE_PMAX9OM_kr&_nc_ht=scontent.fbho5-1.fna&oh=8ff9371f3e40d6d139118258a3c12f43&oe=60CDD9D9" />
+                    </a>
+                    <svg className="half-circle" viewBox="0 0 106 57">
+                        <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+                    </svg>
+
+                    <div className="author-name">
+                        <div className="author-name-prefix">Author</div>
+            Harsh Vijay Singh
+          </div>
+                </div>
+                <div className="tags">
+                    <a href="#">html</a>
+                    <a href="#">css</a>
+                    <a href="#">web-dev</a>
+                </div>
+            </article>
+
+            <article className="card">
+                <div className="card-header">
+                    <p>Sep 11th 2020</p>
+                    <h2>Never forget</h2>
+                </div>
+
+                <div className="card-author">
+                    <a className="author-avatar" href="#">
+                        <img src="https://scontent.fbho5-1.fna.fbcdn.net/v/t1.6435-9/168066427_1895907330549568_6597227817058193863_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=PjuyJsAE_PMAX9OM_kr&_nc_ht=scontent.fbho5-1.fna&oh=8ff9371f3e40d6d139118258a3c12f43&oe=60CDD9D9" />
+                    </a>
+                    <svg className="half-circle" viewBox="0 0 106 57">
+                        <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+                    </svg>
+
+                    <div className="author-name">
+                        <div className="author-name-prefix">Author</div>
+            Harsh Vijay Singh
+          </div>
+                </div>
+                <div className="tags">
+                    <a href="#">html</a>
+                    <a href="#">css</a>
+                    <a href="#">web-dev</a>
+                </div>
+            </article>
+
+            <article className="card">
+                <div className="card-header">
+                    <p>Sep 11th 2020</p>
+                    <h2>Never forget</h2>
+                </div>
+
+                <div className="card-author">
+                    <a className="author-avatar" href="#">
+                        <img src="https://scontent.fbho5-1.fna.fbcdn.net/v/t1.6435-9/168066427_1895907330549568_6597227817058193863_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=PjuyJsAE_PMAX9OM_kr&_nc_ht=scontent.fbho5-1.fna&oh=8ff9371f3e40d6d139118258a3c12f43&oe=60CDD9D9" />
+                    </a>
+                    <svg className="half-circle" viewBox="0 0 106 57">
+                        <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+                    </svg>
+
+                    <div className="author-name">
+                        <div className="author-name-prefix">Author</div>
+            Harsh Vijay Singh
+          </div>
+                </div>
+                <div className="tags">
+                    <a href="#">html</a>
+                    <a href="#">css</a>
+                    <a href="#">web-dev</a>
+                </div>
+            </article>
+
+            <article className="card">
+                <div className="card-header">
+                    <p>Sep 11th 2020</p>
+                    <h2>Never forget</h2>
+                </div>
+
+                <div className="card-author">
+                    <a className="author-avatar" href="#">
+                        <img src="https://scontent.fbho5-1.fna.fbcdn.net/v/t1.6435-9/168066427_1895907330549568_6597227817058193863_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=PjuyJsAE_PMAX9OM_kr&_nc_ht=scontent.fbho5-1.fna&oh=8ff9371f3e40d6d139118258a3c12f43&oe=60CDD9D9" />
+                    </a>
+                    <svg className="half-circle" viewBox="0 0 106 57">
+                        <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+                    </svg>
+
+                    <div className="author-name">
+                        <div className="author-name-prefix">Author</div>
+            Harsh Vijay Singh
+          </div>
+                </div>
+                <div className="tags">
+                    <a title="html" href="https://www.linkedin.com/in/harsh-vijay-singh-0303b482/">html</a>
+                    <a href="https://www.linkedin.com/in/harsh-vijay-singh-0303b482/">css</a>
+                    <a href="https://www.linkedin.com/in/harsh-vijay-singh-0303b482/">web-dev</a>
+                </div>
+            </article>
+
+            <article className="card">
+                <div className="card-header">
+                    <p>Sep 11th 2020</p>
+                    <h2>Never forget</h2>
+                </div>
+
+                <div className="card-author">
+                    <a className="author-avatar" href="#">
+                        <img src="https://scontent.fbho5-1.fna.fbcdn.net/v/t1.6435-9/168066427_1895907330549568_6597227817058193863_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=PjuyJsAE_PMAX9OM_kr&_nc_ht=scontent.fbho5-1.fna&oh=8ff9371f3e40d6d139118258a3c12f43&oe=60CDD9D9" />
+                    </a>
+                    <svg className="half-circle" viewBox="0 0 106 57">
+                        <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+                    </svg>
+
+                    <div className="author-name">
+                        <div className="author-name-prefix">Author</div>
+            Harsh Vijay Singh
+          </div>
+                </div>
+                <div className="tags">
+                    <a href="#">html</a>
+                    <a href="#">css</a>
+                    <a href="#">web-dev</a>
+                </div>
+            </article>
+
+            <article className="card">
+                <div className="card-header">
+                    <p>Sep 11th 2020</p>
+                    <h2>Never forget</h2>
+                </div>
+
+                <div className="card-author">
+                    <a className="author-avatar" href="#">
+                        <img src="https://scontent.fbho5-1.fna.fbcdn.net/v/t1.6435-9/168066427_1895907330549568_6597227817058193863_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=PjuyJsAE_PMAX9OM_kr&_nc_ht=scontent.fbho5-1.fna&oh=8ff9371f3e40d6d139118258a3c12f43&oe=60CDD9D9" />
+                    </a>
+                    <svg className="half-circle" viewBox="0 0 106 57">
+                        <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+                    </svg>
+
+                    <div className="author-name">
+                        <div className="author-name-prefix">Author</div>
+            Harsh Vijay Singh
+          </div>
+                </div>
+                <div className="tags">
+                    <a href="#">html</a>
+                    <a href="#">css</a>
+                    <a href="#">web-dev</a>
+                </div>
+            </article>
+
+
+
+
+        </div>
+    )
+}
+
+export default Card
